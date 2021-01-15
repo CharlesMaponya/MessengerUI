7ea8acd1c69ca1b6a2653171be932ae08245d2ff
CharlesMaponya <maponyacl@gmail.com>
2021-01-15 20:21:39 +0200

    Front -end Message theme configuration

---
+++ Components/ActiveUserComponent.cs
@@ -0,0 +1,34 @@
+﻿using MessengerUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerUI.Components
+{
+    public class ActiveUserComponent : ViewComponent
+
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ActiveUserComponent(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var activeUser = await _userManager.GetUserAsync((System.Security.Claims.ClaimsPrincipal)User);
+
+            var model = new ActiveUserModel
+            {
+                Name = activeUser.Name,
+                Surname = activeUser.Surname,
+                Image = activeUser.ImageUrl
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Models/ActiveUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerUI.Models
+{
+    public class ActiveUserModel
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Image { get; set; }
+    }
+}
